fa2fc788018030bee6bb53bc6d80be680bae54e8
Almir Ishmukhametov <alimfyl@gmail.com>
2026-07-21 22:32:01 +0500

    добавлен обработчик HTTP запросов

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
-const http = require('http');
+const http = require("http");
+const { getUsers } = require("./modules/users");
+
+const host = "127.0.0.1";
+const port = process.env.PORT || 3003;
 
 const server = http.createServer((request, response) => {
+  const url = new URL(request.url, `http://${host}`);
+  const params = url.searchParams;
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+  if (params.size === 0) {
+    response.writeHead(200, {
+      "Content-Type": "text/plain; charset=utf-8",
+    });
+    response.end("Hello, World!");
+    return;
+  }
 
+  if (params.size !== 1) {
+    response.writeHead(500, {
+      "Content-Type": "text/plain; charset=utf-8",
+    });
+    response.end();
+    return;
+  }
+
+  if (params.has("hello")) {
+    const name = params.get("hello");
+
+    if (!name) {
+      response.writeHead(400, {
+        "Content-Type": "text/plain; charset=utf-8",
+      });
+      response.end("Enter a name");
+      return;
+    }
+
+    response.writeHead(200, {
+      "Content-Type": "text/plain; charset=utf-8",
+    });
+    response.end(`Hello, ${name}.`);
+    return;
+  }
+
+  if (params.has("users")) {
+    getUsers((error, users) => {
+      if (error) {
+        response.writeHead(500, {
+          "Content-Type": "text/plain; charset=utf-8",
+        });
+        response.end();
+        return;
+      }
+
+      response.writeHead(200, {
+        "Content-Type": "application/json; charset=utf-8",
+      });
+      response.end(users);
+    });
+
+    return;
+  }
+
+  response.writeHead(500, {
+    "Content-Type": "text/plain; charset=utf-8",
+  });
+  response.end();
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`);
 });
